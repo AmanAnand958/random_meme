68c1e80cdc585559cf930fcfbe409cdc6b211367
Aman Anand <amananand@Amans-MacBook-Air.local>
2024-05-23 14:47:11 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
       </h1>
       <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
         <Random />
+        <Tag/>
         
       </div>
     </div>
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,32 +1,10 @@
+import useGif from "../hooks/useGif";
+import Spinner from "./Spinner";
 
-import {useEffect, useState} from "react";
-import axios from "axios";
-import Spinner from "./Spinner"
 const Random = () => {
-  const API_KEY=process.env.REACT_APP_GIPHY_API_KEY;
-  const [gif,setGif]=useState("");
-  const url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-  const[Loading,setLoading]=useState(false);
-
-  async function fetchData(){
-    setLoading(true);
-    const {data}=await axios.get(url);
-    const imgSource=data.data.images.downsized_large.url;
-    setGif(imgSource);
-    setLoading(false);
-
-  }
-
-
-  useEffect(()=>{
-    fetchData();
-  },[]);
-
-  function clickHandler(){
-    fetchData();
-  }
-
+  const [gif,Loading,fetchData]=useGif();
   
+
   return (
     <div className="'w-1/2  bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
       <h1 className=" mt-[15px] text-2xl underline uppercase font-bold">Random Gif</h1>
@@ -34,7 +12,7 @@ const Random = () => {
         Loading?(<Spinner/>):(<img src={gif} alt="gif" width="450" />)
       }
       
-      <button className="w-10/12 bg-yellow-200 opacity-30 py-2 rounded-lg mb-[20px]"onClick={clickHandler}>Generate</button>
+      <button className="w-10/12 bg-yellow-400 opacity-50 py-2 rounded-lg mb-[20px]"onClick={()=>fetchData()}>Generate</button>
     </div>
   );
 };
